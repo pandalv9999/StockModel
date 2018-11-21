ac15e0b9852a42957f9906803568fbd15876a9bc
Simon <simonwux@126.com>
2018-11-20 17:03:05 -0800

    Rewrite the api test.

--- test/model/AlphaVantageImplTest.java
+++ test/model/AlphaVantageImplTest.java
@@ -1,13 +1,20 @@
 package model;
 
+import org.junit.Before;
+
 import static junit.framework.TestCase.fail;
 import static org.junit.Assert.assertEquals;
 
 public class AlphaVantageImplTest {
 
+  AlphaVantageImpl a;
+  @Before
+  public void setUp() {
+    a = AlphaVantageImpl.getInstance();
+  }
+
   @org.junit.Test
-  public void apiTest() {
-    AlphaVantageImpl a = AlphaVantageImpl.getInstance();
+  public void searchCode() {
     try {
       a.searchCode("asdf");
       fail("There is no code for this company.");
@@ -16,10 +23,14 @@ public class AlphaVantageImplTest {
     }
     assertEquals("GOOG", a.searchCode("Google"));
     assertEquals("MSFT", a.searchCode("microsoft"));
+  }
 
-
+  @org.junit.Test
+  public void getOpenPrice() {
     // Test getting an open price.
     double price = a.getOpenPrice("GOOG", "2018-11-09");
+    assertEquals(1073.99, price, 0.01);
+    price = a.getPrice("GOOG", "2018-11-09", "open");
     assertEquals(1073.99, price, 0.01);
     try {
       a.getHighPrice("asdfasdfasdf", "2018-11-09");
@@ -34,9 +45,14 @@ public class AlphaVantageImplTest {
     } catch (IllegalArgumentException e) {
       // This should be empty.
     }
+  }
 
+  @org.junit.Test
+  public void getHighPrice() {
     // Test getting a high price.
-    price = a.getHighPrice("GOOG", "2018-11-09");
+    Double price = a.getHighPrice("GOOG", "2018-11-09");
+    assertEquals(1075.56, price, 0.01);
+    price = a.getPrice("GOOG", "2018-11-09", "high");
     assertEquals(1075.56, price, 0.01);
     try {
       a.getHighPrice("asdfasdfasdf", "2018-11-09");
@@ -51,9 +67,14 @@ public class AlphaVantageImplTest {
     } catch (IllegalArgumentException e) {
       // This should be empty.
     }
+  }
 
+  @org.junit.Test
+  public void getLowPrice() {
     // Test getting a low price.
-    price = a.getLowPrice("GOOG", "2018-11-09");
+    double price = a.getLowPrice("GOOG", "2018-11-09");
+    assertEquals(1053.11, price, 0.01);
+    price = a.getPrice("GOOG", "2018-11-09", "low");
     assertEquals(1053.11, price, 0.01);
     try {
       a.getLowPrice("asdfasdfasdf", "2018-11-09");
@@ -68,9 +89,14 @@ public class AlphaVantageImplTest {
     } catch (IllegalArgumentException e) {
       // This should be empty.
     }
+  }
 
+  @org.junit.Test
+  public void getClosePrice() {
     // Test getting a close price.
-    price = a.getClosePrice("GOOG", "2018-11-09");
+    double price = a.getClosePrice("GOOG", "2018-11-09");
+    assertEquals(1066.15, price, 0.01);
+    price = a.getPrice("GOOG", "2018-11-09", "close");
     assertEquals(1066.15, price, 0.01);
 
     try {
@@ -86,7 +112,11 @@ public class AlphaVantageImplTest {
     } catch (IllegalArgumentException e) {
       // This should be empty.
     }
+  }
 
+
+  @org.junit.Test
+  public void getVolume() {
     // Test getting a volume
     Double volume = a.getVolume("GOOG", "2018-11-09");
     assertEquals(1343313.0, volume, 0.01);
